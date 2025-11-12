a9c82b748eef36146ae77525ff481ae8412d2709
HaDangQuang <hadangquang1408@gmail.com>
2025-11-12 10:34:52 +0700

    feat: Add route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "../pages/HomePage.tsx";
+import Home from "../pages/home-page";
+import LoginPage from "../pages/login-page";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <LoginPage /> },
 ]);
